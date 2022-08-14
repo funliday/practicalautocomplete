a8d231b2e18fe862165e529dcabf658258a23e97
kewang <cpckewang@gmail.com>
2022-08-14 11:11:20 +0000

    Deploying to gh-pages from @ practicalautocomplete/practicalautocomplete@fe7180e567e7795dac29fcda18db632c77a67e0c 🚀

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,29 @@
 * Copyright 2013-2022 Kewang
 * Licensed under MIT (https://github.com/StartBootstrap/practical-autocomplete/blob/master/LICENSE)
 */
-// 
+/* eslint-env browser */
+//
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector('#sidebarToggle');
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
+  if (sidebarToggle) {
+    // Uncomment Below to persist sidebar toggle between refreshes
+    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+    //     document.body.classList.toggle('sb-sidenav-toggled');
+    // }
+    sidebarToggle.addEventListener('click', event => {
+      event.preventDefault();
 
+      document.body.classList.toggle('sb-sidenav-toggled');
+
+      localStorage.setItem(
+        'sb|sidebar-toggle',
+        document.body.classList.contains('sb-sidenav-toggled')
+      );
+    });
+  }
 });
