fe7180e567e7795dac29fcda18db632c77a67e0c
kewang <cpckewang@gmail.com>
2022-08-14 19:10:45 +0800

    Add GitHub logo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,21 +1,26 @@
-// 
+/* eslint-env browser */
+//
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector('#sidebarToggle');
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
+  if (sidebarToggle) {
+    // Uncomment Below to persist sidebar toggle between refreshes
+    // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+    //     document.body.classList.toggle('sb-sidenav-toggled');
+    // }
+    sidebarToggle.addEventListener('click', event => {
+      event.preventDefault();
 
+      document.body.classList.toggle('sb-sidenav-toggled');
+
+      localStorage.setItem(
+        'sb|sidebar-toggle',
+        document.body.classList.contains('sb-sidenav-toggled')
+      );
+    });
+  }
 });
